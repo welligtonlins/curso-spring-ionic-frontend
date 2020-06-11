eddac7ee6ad549ec9cbbc4fdf2eca8b6baf3fa99
Welligton Lins <welligtonlinsufac@gmail.com>
2020-06-11 17:48:32 -0300

    Comecando a implementacao  a autenticacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
 import { MyApp } from './app.component';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { CategoriaService } from '../services/domain/categoria.service';
 import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
+import { AuthService } from '../services/auth.service';
 
 @NgModule({
   declarations: [
@@ -28,7 +27,8 @@ import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     CategoriaService,
-    ErrorInterceptorProvider
+    ErrorInterceptorProvider,
+    AuthService
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, IonicPage, MenuController } from 'ionic-angular';
+import { NavController, IonicPage } from 'ionic-angular';
+import { MenuController } from 'ionic-angular/components/app/menu-controller';
 import { CredenciaisDTO } from '../../models/credenciais.dto';
+import { AuthService } from '../../services/auth.service';
 
 @IonicPage()
 @Component({
@@ -8,26 +10,32 @@ import { CredenciaisDTO } from '../../models/credenciais.dto';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
   creds : CredenciaisDTO = {
-    email : "",
-    senha : ""
+    email: "",
+    senha: ""
   };
-  constructor(public navCtrl: NavController, public menu: MenuController) {
+
+  constructor(
+    public navCtrl: NavController, 
+    public menu: MenuController,
+    public auth: AuthService) {
 
   }
-  
+
   ionViewWillEnter() {
     this.menu.swipeEnable(false);
   }
-
+    
   ionViewDidLeave() {
     this.menu.swipeEnable(true);
   }
 
-  login(){
-    console.log(this.creds);
-    this.navCtrl.setRoot('CategoriasPage');
+  login() {
+    this.auth.authenticate(this.creds)
+      .subscribe(response => {
+        console.log(response.headers.get('Authorization'));
+        this.navCtrl.setRoot('CategoriasPage');
+      },
+      error => {});    
   }
-
 }
