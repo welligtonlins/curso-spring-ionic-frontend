b3252c1786e8ce4eea92c7f0968d143c9d26470d
Welligton Lins <welligtonlinsufac@gmail.com>
2020-08-17 16:01:55 -0300

    Povoando dinamicamente estado e cidade

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CidadeService } from '../../services/domain/cidade.service';
+import { EstadoService } from '../../services/domain/estado.service';
+import { CidadeDTO } from '../../models/cidade.dto';
+import { EstadoDTO } from '../../models/estado.dto';
+import { onErrorResumeNext } from 'rxjs/operator/onErrorResumeNext';
 
 @IonicPage()
 @Component({
@@ -8,12 +13,17 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   templateUrl: 'signup.html',
 })
 export class SignupPage {
+
 formGroup: FormGroup;
+cidades: CidadeDTO[];
+estados: EstadoDTO[];
 
-
-  constructor(public navCtrl: NavController,
+  constructor(
+    public navCtrl: NavController,
     public navParams: NavParams,
-    public formBuilder: FormBuilder) {
+    public formBuilder: FormBuilder,
+    public cidadeService: CidadeService,
+    public estadoService: EstadoService){
 
       this.formGroup = this.formBuilder.group({
         nome: ['Joaquim', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
@@ -33,6 +43,26 @@ formGroup: FormGroup;
         cidadeId : [null, [Validators.required]] 
       });
   }
+
+  ionViewDidLoad(){
+    this.estadoService.findAll()
+      .subscribe(Response => {
+        this.estados = Response;
+        this.formGroup.controls.estadoId.setValue(this.estados[0].id);
+        this.updateCidades();
+      },
+      error => {});
+  }
+
+  updateCidades(){
+    let estado_id = this.formGroup.value.estadoId;
+    this.cidadeService.findAll(estado_id)
+    .subscribe(response => {
+      this.cidades = response;
+      this.formGroup.controls.cidadeId.setValue(null);
+    },
+    error => {});
+  }  
 
   signupUser() {
     console.log("Formulario enviado");
